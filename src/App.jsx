3589f350424d1f9dcacf3f750c4e15b23b9ae206
ranjit95rkmks <36200163+ranjit95rkmks@users.noreply.github.com>
2023-07-18 13:29:57 +0600

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ContactForm from "./component/ContactForm";
+
+
+
+const App = () => {
+  return (
+    <div>
+      {/* Other components or content */}
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App
---
+++ src/component/ContactForm.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your form validation logic here
+    const validationErrors = {};
+    if (formData.name.trim() === '') {
+      validationErrors.name = 'Name is required';
+    }
+    if (formData.email.trim() === '') {
+      validationErrors.email = 'Email is required';
+    }
+    if (formData.message.trim() === '') {
+      validationErrors.message = 'Message is required';
+    }
+
+    setErrors(validationErrors);
+
+    // If there are no validation errors, you can proceed with form submission or other actions
+    if (Object.keys(validationErrors).length === 0) {
+      // Perform form submission or other actions here
+      console.log('Form submitted:', formData);
+    }
+  };
+
+  return (
+    <div className="container mx-auto px-4 mt-8">
+      <form className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md" onSubmit={handleSubmit}>
+        {/* Form elements */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name:</label>
+          <input
+            className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.name && 'border-red-500'}`}
+            id="name"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+          />
+          {errors.name && <p className="text-red-500 text-xs italic">{errors.name}</p>}
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email:</label>
+          <input
+            className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.email && 'border-red-500'}`}
+            id="email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Enter your email address"
+          />
+          {errors.email && <p className="text-red-500 text-xs italic">{errors.email}</p>}
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">Message:</label>
+          <textarea
+            className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.message && 'border-red-500'}`}
+            id="message"
+            rows="4"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Enter your message"
+          />
+          {errors.message && <p className="text-red-500 text-xs italic">{errors.message}</p>}
+        </div>
+        <div className="flex items-center justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
